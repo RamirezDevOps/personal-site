6a2595e604f1a936d5fd0e50f259503e4695ce0d
RamirezDevOps <anderson@codinginitiative.org>
2025-03-09 01:58:07 -0500

    fixed navigation

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,17 +3,17 @@
 
 
     let tabs = [
-        {name: 'Projects', link: '#projects'},
-        {name: 'About me', link: '#about'},
+        {name: 'Projects', link: '/#projects'},
+        {name: 'About me', link: '/#about'},
     ];
 </script>
 
 <header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " + (
     y > 0 ? ' py-4 bg-slate-950 border-violet-950' : ' py-6 bg-transparent border-transparent'
 )}>
-    <h1 class="font-medium">
+    <a href="/"><h1 class="font-medium">
         <b class="font-bold poppins">Anderson</b> Ramirez
-    </h1>
+    </h1></a>
     <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a href={tab.link} class="duration-200 hover:text-violet-400">
@@ -21,7 +21,7 @@
             </a>
         {/each}
     </div>
-    <a href="#footer" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
+    <a href="/#footer" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
         <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
         <h4 class="relative z-9">Get in touch &rarr;</h4>
     </a>
